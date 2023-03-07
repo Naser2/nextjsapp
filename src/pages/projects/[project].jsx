d07e8cc63d247cc6b3b3d233193f6f08da917637
Naser2 <nassersanou23@gmail.com>
2023-03-07 23:37:14 +0100

    cleaning up jsx [replaced all a tags-2]

--- src/pages/projects/[project].jsx
+++ src/pages/projects/[project].jsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Card } from '@/components/Card'
 import { Section } from '@/components/Section'
 import { SimpleLayout } from '@/components/SimpleLayout'
+import Link from 'next/link'
 
 const Project = function ({ products }) {
   return (
